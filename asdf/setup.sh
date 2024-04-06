8e9ad7c263e4602ec38a9bd45da08bbecbfb0f0c
FUKUOkA Keito <keito.0o0.kirby@gmail.com>
2024-04-06 05:37:28 +0000

    add asdf setup.

---
+++ asdf/setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+: <<EOF_COMMENT
+-----------------------------------------------------------
+Install asdf
+
+If asdf is already installed, returns error "1".
+If this os isn't macos or Linux, returns error "1".
+If shell isn't bash, returns error "1".
+
+Date Created: 2024/01/14: FUKUOKA Keito
+-----------------------------------------------------------
+EOF_COMMENT
+
+# -e: Interrupts the script when an error occurs.
+# -u: Error on undefined variables
+# -o pipefail: Script is interrupted when an error in the middle of pipe-combined command.
+set -euo pipefail
+
+# Make sure that asdf is already installed.
+if [ $(which asdf) ]; then
+  echo 'asdf is already installed.' 1>&2
+  exit 1
+fi
+
+# Make sure that shell is bash.
+if [ "${SHELL##*/}" != 'bash' ]; then
+  echo 'This script is only bash.'
+  exit 1
+fi
+
+# Download asdf
+git clone https://github.com/asdf-vm/asdf.git ${HOME}/.asdf
+
+# settings
+asdf_target=(
+  'source ${HOME}/.asdf/asdf.sh'
+  'source ${HOME}/.asdf/completions/asdf.bash'
+)
+if [ "$(uname)" == 'Darwin' ]; then
+  for each in "${asdf_target[@]}"; do
+    eval ${each}
+    echo ${each} >>"${HOME}/.bash_profile"
+  done
+elif [ "$(uname)" == 'Linux' ]; then
+  for each in "${asdf_target[@]}"; do
+    eval ${each}
+    echo ${each} >>"${HOME}/.bashrc"
+  done
+else
+  echo "$(uname) is not supported." 1>&2
+  exit 1
+fi
+
+echo 'Installation of asdf is compete!!!!!!'
+exit 0
