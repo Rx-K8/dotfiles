c910e68d201430e080e3396d88d358dfc51688c2
FUKUOKA Keito <keito.0o0.kirby@gmail.com>
2024-04-17 13:45:27 +0900

    add Copilot in neovim

--- neovim/nvchad/lua/mappings.lua
+++ neovim/nvchad/lua/mappings.lua
@@ -16,4 +16,22 @@ map("n", "<leader>J", function()
   require("flash").treesitter()
 end, { desc = "Flash Treesitter" })
 
+-- copilt chat
+map("n", "<leader>cq", function()
+  local input = vim.fn.input "Quick Chat: "
+  if input ~= "" then
+    require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+  end
+end, { desc = "CopilotChat - Quick chat" })
+
+map("n", "<leader>ch", function()
+  local actions = require "CopilotChat.actions"
+  require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+end, { desc = "CopilotChat - Help actions" })
+
+map("n", "<leader>cp", function()
+  local actions = require "CopilotChat.actions"
+  require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+end, { desc = "CopilotChat - Prompt actions" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
